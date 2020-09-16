a8f1ed5fcc6b3b74fb53b844fac88e2e18fe07b7
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-16 08:21:26 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ping_stat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
