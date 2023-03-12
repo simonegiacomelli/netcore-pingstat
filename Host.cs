65f437d30a062326d55d28c50d2c58a133d76949
Simone <simone.giacomelli@gmail.com>
2023-03-12 08:56:48 +0000

    non root ping

--- Host.cs
+++ Host.cs
@@ -9,38 +9,22 @@ namespace PingStat
         internal bool PingSuccess;
         internal long Time { get; set; }
         
-        private static byte[] _buffer = new byte[32];
-        static readonly PingOptions Options = new PingOptions();
-
         Ping _pingSender;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        static Host()
-        {
-            for (int idx = 0; idx < _buffer.Length; idx++)
-                _buffer[idx] = (byte) 'a';
-        }
 
         public void Ping()
         {
-            if (_pingSender == null)
-            {
-                _pingSender = new Ping();
-                Options.DontFragment = true;
-            }
-            
+            _pingSender ??= new Ping();
+
             try
             {
-                PingReply reply = _pingSender.Send(Name, 1500, _buffer, Options);
+                PingReply reply = _pingSender.Send(Name, 1500);
                 PingSuccess = reply.Status == IPStatus.Success;
                 Time = reply.RoundtripTime;
             }
             catch (Exception ex)
             {
                 PingSuccess = false;
-            }  
+            }
         }
 
     }
